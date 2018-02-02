f1081cb8b705cb18526d7d4fe60cbd9926e0e678
Stefano Pigozzi <ste.pigozzi@gmail.com>
2018-02-02 11:46:27 +0100

    Stuff

--- webserver.py
+++ webserver.py
@@ -28,6 +28,13 @@ def page_leaderboards():
     session.close()
     return render_template("leaderboards.html", dota_data=dota_data, rl_data=rl_data, ow_data=ow_data, osu_data=osu_data, lol_data=lol_data)
 
+@app.route("/challenge/1")
+def page_challenge_one():
+    session = Session()
+    result = session.execute(r"SELECT sum(osu.std_pp) + sum(osu.taiko_pp) + sum(osu.catch_pp) + sum(osu.mania_pp) total_pp FROM osu;")
+    session.close()
+    return render_template("challenge1.html", starting=4959.518703999999, result=result, target=5200)
+
 
 if __name__ == "__main__":
     try:
