cf7f82adc5182afe3ce78a1b623834d09189a8be
Stefano Pigozzi <ste.pigozzi@gmail.com>
2018-10-09 19:35:27 +0200

    new rm

--- discordbot.py
+++ discordbot.py
@@ -45,9 +45,7 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 
 # Radio messages
-radio_messages = ["https://www.youtube.com/watch?v=3-yeK1Ck4yk",
-                  "https://youtu.be/YcR7du_A1Vc",
-                  "https://clyp.it/byg3i52l"]
+radio_messages = ["https://www.youtube.com/watch?v=tm0ozXhits4"]
 
 song_special_messages = {
     "despacito": ":arrow_forward: this is so sad. alexa play {song}",
