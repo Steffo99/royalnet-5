65272f785a3dc567f9048e82c94310956a6884cc
Ubuntu <ubuntu@ip-172-31-33-190.eu-central-1.compute.internal>
2018-11-07 19:45:04 +0100

    temp

--- update.sh
+++ update.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
+# Requires SENTRY_AUTH_TOKEN and SENTRY_ORG set in .profile
+
+old=$(git rev-list HEAD -n 1)
 git pull
+new=$(git rev-list HEAD -n 1)
+if [ ${old} = ${new} ]; then
+        version=$(sentry-cli releases propose-version)
+        sentry-cli releases new -p royalnet ${version}
+        sentry-cli releases set-commits --auto ${version}
+fi
 sudo python3.6 -m pip install -r requirements.txt
 sudo service apache2 restart
 python3.6 -OO bots.py
