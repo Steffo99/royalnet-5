07af7d0c6b91934452082c15e1768993a12e137d
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-03-15 14:48:02 +0100

    Add smecds command

--- royalnet/commands/__init__.py
+++ royalnet/commands/__init__.py
@@ -1,5 +1,6 @@
 from .null import NullCommand
 from .ping import PingCommand
 from .ship import ShipCommand
+from .smecds import SmecdsCommand
 
-__all__ = ["NullCommand", "PingCommand", "ShipCommand"]
+__all__ = ["NullCommand", "PingCommand", "ShipCommand", "SmecdsCommand"]
