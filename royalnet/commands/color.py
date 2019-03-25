015d459791dd32bf30ae9eeba6cf5c211be5fa15
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-03-25 10:03:50 +0100

    Add color command

---
+++ royalnet/commands/color.py
@@ -0,0 +1,12 @@
+from ..utils import Command, CommandArgs, Call
+
+
+class PingCommand(Command):
+
+    command_name = "color"
+    command_title = "Invia un colore in chat...?"
+
+    async def common(self, call: Call, args: CommandArgs):
+        await call.reply("""
+        <i>I am sorry, unknown error occured during working with your request, Admin were notified</i>
+        """)
