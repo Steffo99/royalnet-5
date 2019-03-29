ea88c54b48e491bfc0b3939664a41dfdcf7dd7c1
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-03-29 12:14:23 +0100

    I'm lost

---
+++ royalnet/database/alchemy.py
@@ -0,0 +1,5 @@
+import typing
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
