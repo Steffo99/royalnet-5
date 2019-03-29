38baa29fc36a08c7ab9ed60f0890cf66cdb0a6de
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-03-29 12:37:46 +0100

    ???

---
+++ royalnet/database/TODO.md
@@ -0,0 +1,3 @@
+# TODO
+
+L'obiettivo per il sottopackage `database` è quello di creare una classe `Alchemy`.
--- royalnet/database/alchemy.py
+++ royalnet/database/alchemy.py
@@ -3,3 +3,4 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
