571f381c7502135d29f89612a91bf77a4b5568a5
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-03-29 15:48:13 +0100

    Generalize alchemy

--- royalnet/database/alchemy.py
+++ royalnet/database/alchemy.py
@@ -1,17 +1,19 @@
+import typing
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .tables import Royal
 from ..utils import classdictjanitor
 
 
 class Alchemy:
-    def __init__(self, database_uri: str = "sqlite://"):
+    def __init__(self, database_uri: str = "sqlite://", tables: typing.Optional[typing.List] = None):
         self.engine = create_engine(database_uri)
         self.Base = declarative_base(bind=self.engine)
         self.Session = sessionmaker(bind=self.engine)
-        self._create_tables()
+        self._create_tables(tables)
 
-    def _create_tables(self):
-        self.Royal = type("Royal", (self.Base,), classdictjanitor(Royal))
+    def _create_tables(self, tables: typing.Optional[typing.List]):
+        for table in tables:
+            name = table.__name__
+            self.__setattr__(name, type(name, (self.Base,), classdictjanitor(table)))
         self.Base.metadata.create_all()
