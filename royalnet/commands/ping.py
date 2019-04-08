23594fc4ac9abf59a5269b24422a8b25711bba27
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-04-08 10:47:57 +0200

    Create delayed ping

--- royalnet/commands/ping.py
+++ royalnet/commands/ping.py
@@ -1,11 +1,19 @@
-from ..utils import Command, CommandArgs, Call
+import asyncio
+from ..utils import Command, CommandArgs, Call, InvalidInputError
 
 
 class PingCommand(Command):
 
     command_name = "ping"
     command_description = "Ping pong!"
-    command_syntax = ""
+    command_syntax = "[time_to_wait]"
 
     async def common(self, call: Call):
+        try:
+            time = int(call.args[0])
+        except InvalidInputError:
+            time = 0
+        except ValueError:
+            raise InvalidInputError("time_to_wait is not a number")
+        await asyncio.sleep(time)
         await call.reply("🏓 Pong!")
