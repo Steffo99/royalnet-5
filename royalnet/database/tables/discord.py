be1d35f2133bfff586fe0efc29a8314641b93f76
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-04-11 18:56:01 +0200

    Create Discord users table

---
+++ royalnet/database/tables/discord.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, \
+                       Integer, \
+                       String, \
+                       BigInteger, \
+                       ForeignKey
+from sqlalchemy.orm import relationship
+from .royals import Royal
+
+
+class Discord:
+    __tablename__ = "discord"
+
+    royal_id = Column(Integer, ForeignKey("royals.uid"))
+    discord_id = Column(BigInteger, primary_key=True)
+    username = Column(String)
+    discriminator = Column(String)
+    avatar_hash = Column(String)
+
+    royal = relationship("Royal", backref="telegram")
+
+    def __repr__(self):
+        return f"<Discord {str(self)}>"
+
+    def __str__(self):
+        return f"[c]discord:{self.full_username()}[/c]"
+
+    def full_username(self):
+        return f"{self.username}#{self.discriminator}"
