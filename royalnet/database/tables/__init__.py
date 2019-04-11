d3ee249bc99d0987bc79c05b61711e63ea0d08d4
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-04-11 18:56:01 +0200

    Create Discord users table

--- royalnet/database/tables/__init__.py
+++ royalnet/database/tables/__init__.py
@@ -5,5 +5,6 @@ from .aliases import Alias
 from .activekvgroup import ActiveKvGroup
 from .keyvalue import Keyvalue
 from .keygroup import Keygroup
+from .discord import Discord
 
-__all__ = ["Royal", "Telegram", "Diario", "Alias", "ActiveKvGroup", "Keyvalue", "Keygroup"]
+__all__ = ["Royal", "Telegram", "Diario", "Alias", "ActiveKvGroup", "Keyvalue", "Keygroup", "Discord"]
