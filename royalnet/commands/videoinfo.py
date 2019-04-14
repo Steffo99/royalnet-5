9ecc8cf95953dcb2456be7a5198486dbdf90c0a1
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-04-14 18:58:06 +0200

    Add a few more properties

--- royalnet/commands/videoinfo.py
+++ royalnet/commands/videoinfo.py
@@ -16,9 +16,18 @@ class VideoinfoCommand(Command):
         for info in info_list:
             info_dict = info.__dict__
             message = f"🔍 Dati di [b]{info}[/b]:\n"
-            for key in __dict__:
+            for key in info_dict:
+                # Skip description
+                if key == "description":
+                    continue
+                # Skip formats
+                if key == "formats":
+                    continue
+                if key == "requested_formats":
+                    continue
+                # Skip empty keys
                 if info_dict[key] is None:
                     continue
-                message += f"{key}: [b]{info_dict[key]}[/b]\n"
+                message += f"[c]{key}[/c]: {info_dict[key]}\n"
             await call.reply(message)
             await asyncio.sleep(0.2)
