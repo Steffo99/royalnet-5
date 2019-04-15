8cd9254ebe129ffb17e83dae8032819c79a4ac21
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-04-15 11:39:34 +0200

    Complete summon command

--- royalnet/commands/summon.py
+++ royalnet/commands/summon.py
@@ -1,4 +1,5 @@
 import typing
+import discord
 from ..utils import Command, Call
 from ..network import Message
 
@@ -34,3 +35,37 @@ class SummonCommand(Command):
             await call.reply(f"✅ Mi sono connesso in [c]#{channel_name}[/c].")
             return
         raise TypeError(f"Received unexpected response type while summoning the bot: {response.__class__.__name__}")
+
+    @classmethod
+    async def discord(cls, call: Call):
+        bot = call.interface_obj.bot
+        message: discord.Message = call.kwargs["message"]
+        channel_name: str = call.args.optional(0)
+        if channel_name:
+            guild: typing.Optional[discord.Guild] = message.guild
+            channels: typing.List[discord.abc.GuildChannel]
+            if guild is not None:
+                channels = guild.channels
+            else:
+                channels = bot.get_all_channels()
+            matching_channels: typing.List[discord.VoiceChannel] = []
+            for channel in channels:
+                if isinstance(channel, discord.VoiceChannel):
+                    if channel.name == channel_name:
+                        matching_channels.append(channel)
+            if len(matching_channels) == 0:
+                await call.reply("⚠️ Non esiste alcun canale vocale con il nome specificato.")
+                return
+            elif len(matching_channels) > 1:
+                await call.reply("⚠️ Esiste più di un canale vocale con il nome specificato.")
+                return
+            channel = matching_channels[0]
+        else:
+            author: discord.Member = message.author
+            voice: typing.Optional[discord.VoiceState] = author.voice
+            if voice is None:
+                await call.reply("⚠️ Non sei connesso a nessun canale vocale!")
+                return
+            channel = voice.channel
+        await bot.vc_connect_or_move(channel)
+        await call.reply(f"✅ Mi sono connesso in [c]#{channel.name}[/c].")
