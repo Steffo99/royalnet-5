3621e60deae56c1e7c0debedaef6bab9d804c0c1
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-04-23 16:45:21 +0200

    Add stuff

--- royalnet/commands/play.py
+++ royalnet/commands/play.py
@@ -3,7 +3,7 @@ import asyncio
 import youtube_dl
 import ffmpeg
 from ..utils import Command, Call, NetworkHandler, asyncify
-from ..network import Message, RequestSuccessful, RequestError
+from ..network import Message, RequestSuccessful
 from ..error import TooManyFoundError, NoneFoundError
 from ..audio import RoyalPCMAudio, YtdlInfo
 if typing.TYPE_CHECKING:
@@ -52,6 +52,15 @@ class PlayNH(NetworkHandler):
         return PlaySuccessful(info_list=[source.rpf.info for source in audio_sources])
 
 
+async def notify_on_timeout(call: Call, url: str, time: float, repeat: bool = False):
+    """Send a message after a while to let the user know that the bot is still downloading the files and hasn't crashed."""
+    while True:
+        await asyncio.sleep(time)
+        await call.reply(f"ℹ️ Il download di [c]{url}[/c] sta richiedendo più tempo del solito, ma è ancora in corso!")
+        if not repeat:
+            break
+
+
 class PlayCommand(Command):
     command_name = "play"
     command_description = "Riproduce una canzone in chat vocale."
@@ -62,40 +71,46 @@ class PlayCommand(Command):
     @classmethod
     async def common(cls, call: Call):
         guild, url = call.args.match(r"(?:\[(.+)])?\s*(.+)")
-        response: typing.Union[RequestError, PlaySuccessful] = await call.net_request(PlayMessage(url, guild), "discord")
-        if isinstance(response, RequestError):
+        download_task = loop.create_task(call.net_request(PlayMessage(url, guild), "discord"))
+        notify_task = loop.create_task(notify_on_timeout(call, url, time=20, repeat=True))
+        try:
+            response: PlaySuccessful = await download_task
+        except Exception as exc:
             # RoyalPCMFile errors
-            if isinstance(response.exc, FileExistsError):
-                await call.reply(f"❌ Scaricare [c]{url}[/c] significherebbe sovrascrivere un file già esistente.\nQuesto è un bug, e non dovrebbe mai succedere. Se è appena successo, segnalate il problema a https://github.com/Steffo99/royalnet/issues.\n[p]{response.exc}[/p]")
+            if isinstance(exc, FileExistsError):
+                await call.reply(f"❌ Scaricare [c]{url}[/c] significherebbe sovrascrivere un file già esistente.\nQuesto è un bug, e non dovrebbe mai succedere. Se è appena successo, segnalate il problema a https://github.com/Steffo99/royalnet/issues.\n[p]{exc}[/p]")
                 return
             # ffmpeg errors
-            if isinstance(response.exc, ffmpeg.Error):
-                await call.reply(f"⚠️ Errore durante la conversione a PCM di [c]{url}[/c]:\n[p]{response.exc}[/p]")
+            if isinstance(exc, ffmpeg.Error):
+                await call.reply(f"⚠️ Errore durante la conversione a PCM di [c]{url}[/c]:\n[p]{exc}[/p]")
                 return
             # youtube_dl errors
-            if isinstance(response.exc, youtube_dl.utils.ContentTooShortError):
-                await call.reply(f"⚠️ Mentre era in corso il download di [c]{url}[/c], la connessione è stata interrotta, quindi la riproduzione è stata annullata.\n[p]{response.exc}[/p]")
+            if isinstance(exc, youtube_dl.utils.ContentTooShortError):
+                await call.reply(f"⚠️ Mentre era in corso il download di [c]{url}[/c], la connessione è stata interrotta, quindi la riproduzione è stata annullata.\n[p]{exc}[/p]")
                 return
-            if isinstance(response.exc, youtube_dl.utils.UnavailableVideoError):
-                await call.reply(f"⚠️ Non è disponibile nessun audio su [c]{url}[/c].\n[p]{response.exc}[/p]")
+            if isinstance(exc, youtube_dl.utils.UnavailableVideoError):
+                await call.reply(f"⚠️ Non è disponibile nessun audio su [c]{url}[/c].\n[p]{exc}[/p]")
                 return
-            if isinstance(response.exc, youtube_dl.utils.SameFileError):
-                await call.reply(f"❌ Scaricare [c]{url}[/c] significherebbe scaricare due file diversi sullo stesso nome.\nQuesto è un bug, e non dovrebbe mai succedere. Se è appena successo, segnalate il problema a https://github.com/Steffo99/royalnet/issues.\n[p]{response.exc}[/p]")
+            if isinstance(exc, youtube_dl.utils.SameFileError):
+                await call.reply(f"❌ Scaricare [c]{url}[/c] significherebbe scaricare due file diversi sullo stesso nome.\nQuesto è un bug, e non dovrebbe mai succedere. Se è appena successo, segnalate il problema a https://github.com/Steffo99/royalnet/issues.\n[p]{exc}[/p]")
                 return
-            if isinstance(response.exc, youtube_dl.utils.GeoRestrictedError):
-                await call.reply(f"⚠️ [c]{url}[/c] non può essere visualizzato nel paese in cui si trova il bot e non può essere scaricato.\n[p]{response.exc}[/p]")
+            if isinstance(exc, youtube_dl.utils.GeoRestrictedError):
+                await call.reply(f"⚠️ [c]{url}[/c] non può essere visualizzato nel paese in cui si trova il bot e non può essere scaricato.\n[p]{exc}[/p]")
                 return
-            if isinstance(response.exc, youtube_dl.utils.UnsupportedError):
-                await call.reply(f"⚠️ [c]{url}[/c] non è supportato da YoutubeDL e non può essere scaricato.\n[p]{response.exc}[/p]")
+            if isinstance(exc, youtube_dl.utils.UnsupportedError):
+                await call.reply(f"⚠️ [c]{url}[/c] non è supportato da YoutubeDL e non può essere scaricato.\n[p]{exc}[/p]")
                 return
-            if isinstance(response.exc, youtube_dl.utils.ExtractorError):
-                await call.reply(f"⚠️ Errore nella ricerca di info per [c]{url}[/c]:\n[p]{response.exc}[/p]")
+            if isinstance(exc, youtube_dl.utils.ExtractorError):
+                await call.reply(f"⚠️ Errore nella ricerca di info per [c]{url}[/c]:\n[p]{exc}[/p]")
                 return
-            if isinstance(response.exc, youtube_dl.utils.DownloadError):
-                await call.reply(f"⚠️ Errore nel download di [c]{url}[/c]:\n[p]{response.exc}[/p]")
+            if isinstance(exc, youtube_dl.utils.DownloadError):
+                await call.reply(f"⚠️ Errore nel download di [c]{url}[/c]:\n[p]{exc}[/p]")
                 return
-            if isinstance(response.exc, youtube_dl.utils.YoutubeDLError):
-                await call.reply(f"⚠️ Errore di youtube_dl per [c]{url}[/c]:\n[p]{response.exc}[/p]")
+            if isinstance(exc, youtube_dl.utils.YoutubeDLError):
+                await call.reply(f"⚠️ Errore di youtube_dl per [c]{url}[/c]:\n[p]{exc}[/p]")
                 return
+            raise
+        finally:
+            notify_task.cancel()
         for info in response.info_list:
             await call.reply(f"⬇️ Download di [i]{info.title}[/i] completato.")
--- royalnet/utils/asyncify.py
+++ royalnet/utils/asyncify.py
@@ -4,5 +4,6 @@ import typing
 
 
 async def asyncify(function: typing.Callable, *args, **kwargs):
+    # TODO: make cancellable somehow
     loop = asyncio.get_running_loop()
     return await loop.run_in_executor(None, functools.partial(function, *args, **kwargs))
