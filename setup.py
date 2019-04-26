d2fb8c64ed5e2e9babeb3662f7182b1ebacd9cec
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-04-26 17:49:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,10 +13,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Steffo99/royalnet",
     packages=setuptools.find_packages(),
-    install_requires=[
-        "python-telegram-bot>=11.1.0",
-        "websockets>=7.0"
-    ],
+    install_requires=["python-telegram-bot>=11.1.0",
+                      "websockets>=7.0",
+                      "psycopg2-binary>=2.8",
+                      "aiohttp>=3.5.4",
+                      "sqlalchemy>=1.3.2",
+                      "Markdown>=3.1",
+                      "dateparser>=0.7.1",
+                      "discord.py>=1.0.1",
+                      "youtube_dl>=2019.4.24",
+                      "ffmpeg-python>=0.1.17"],
     python_requires=">=3.7",
     classifiers=[
         "Development Status :: 3 - Alpha",
