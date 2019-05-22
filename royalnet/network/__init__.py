e0e1290ecd778314bc2ef3b67ac642733a295847
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-05-22 02:21:20 +0200

    Split data and request in different files

--- royalnet/network/__init__.py
+++ royalnet/network/__init__.py
@@ -1,5 +1,6 @@
 """Royalnet realated classes."""
-from .data import Data, Request
+from .data import Data
+from .request import Request
 from .package import Package
 from .royalnetlink import RoyalnetLink, NetworkError, NotConnectedError, NotIdentifiedError, ConnectionClosedError
 from .royalnetserver import RoyalnetServer
