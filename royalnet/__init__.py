7b61aff81cc3a392e45753d7d6e645fc226e1ea9
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-05-27 01:06:37 +0200

    Bump version

---
+++ royalnet/__init__.py
@@ -0,0 +1,10 @@
+import audio
+import bots
+import commands
+import database
+import network
+import utils
+
+version = "5.0a10"
+
+__all__ = ["audio", "bots", "commands", "database", "network", "utils"]
