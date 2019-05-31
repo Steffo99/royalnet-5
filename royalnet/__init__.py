aebc42f15702a1b6bbf8b680f708fa19207accf6
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-05-31 14:37:51 +0200

    Bump version

--- royalnet/__init__.py
+++ royalnet/__init__.py
@@ -1,5 +1,5 @@
 from . import audio, bots, commands, database, network, utils, error
 
-version = "5.0a11"
+version = "5.0a12"
 
 __all__ = ["audio", "bots", "commands", "database", "network", "utils", "error"]
