08993451e0b40ce82ec2c58865ca1c47f2121fc8
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-06-01 18:18:51 +0200

    Woah, this is progress

--- royalnet/web/flaskserver.py
+++ royalnet/web/flaskserver.py
@@ -1,15 +1,19 @@
-import typing
 import flask as f
+import os
 from ..database import Alchemy
 
 
-class RoyalFlask:
-    def __init__(self, config_obj: typing.Type):
-        self.app = f.Flask(__name__)
-        self.app.config.from_object(config_obj)
-        self.alchemy = Alchemy(self.app.config["RF_DATABASE_URI"], self.app.config["RF_REQUIRED_TABLES"])
-        for blueprint in self.app.config["RF_BLUEPRINTS"]:
-            self.app.register_blueprint(blueprint)
+app = f.Flask(__name__)
+app.config["RN_ALCHEMY"] = Alchemy(os.environ["DB_PATH"], set())
+with app.app_context():
+    from .alchemyhandler import alchemy_session as db_session
 
-    def debug(self):
-        self.app.run(host="127.0.0.1", port=1234, debug=True)
+
+@app.route("/")
+def test():
+    ...
+    return repr(db_session)
+
+
+if __name__ == "__main__":
+    app.run()
