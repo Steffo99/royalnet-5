59ecc95c1d9d58624144e7a3f5a6e5d2fb8bfb1c
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-06-05 19:45:31 +0200

    more progress

--- royalnet/database/tables/wiki.py
+++ royalnet/database/tables/wiki.py
@@ -1,23 +1,45 @@
 from sqlalchemy import Column, \
                        Integer, \
                        Text, \
-                       String
+                       DateTime
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declared_attr
 # noinspection PyUnresolvedReferences
 from .royals import Royal
 
 
-class Wiki:
-    __tablename__ = "wiki"
+class WikiPage:
+    """Wiki page properties."""
+    __tablename__ = "wikipages"
+
+
+class WikiRevision:
+    """A wiki page revision.
+
+    Warning:
+        Requires PostgreSQL!"""
+    __tablename__ = "wikirevisions"
 
     @declared_attr
     def revision_id(self):
-        return Column(String, primary_key=True)
+        return Column(UUID(as_uuid=True), primary_key=True)
 
     @declared_attr
     def page_id(self):
-        return Column(Integer, nullable=False)
+        return Column(UUID(as_uuid=True), nullable=False)
+
+    @declared_attr
+    def content(self):
+        return Column(Text, nullable=False)
+
+    @declared_attr
+    def timestamp(self):
+        return Column(DateTime, nullable=False)
+
+    @declared_attr
+    def reason(self):
+        return Column(Text)
 
     @declared_attr
     def
