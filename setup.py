f5e2c19a1b1e0e25e59d93c32422186fb554bea1
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-06-06 16:16:51 +0200

    Fix templates not getting distributed correctly

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     description="The great bot network of the Royal Games community",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Steffo99/royalnet",
+    url="https://github.com/royal-games/royalnet",
     packages=setuptools.find_packages(),
     install_requires=["python-telegram-bot>=11.1.0",
                       "websockets>=7.0",
@@ -24,7 +24,10 @@ setuptools.setup(
                       "discord.py>=1.0.1",
                       "youtube_dl>=2019.4.24",
                       "ffmpeg-python>=0.1.17",
-                      "PyNaCl>=1.3.0"],
+                      "PyNaCl>=1.3.0",
+                      "werkzeug>=0.15.4",
+                      "flask>=1.0.3",
+                      "markdown2>=2.3.8"],
     python_requires=">=3.7",
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -35,5 +38,7 @@ setuptools.setup(
         "Topic :: Multimedia :: Sound/Audio",
         "Topic :: Multimedia :: Video",
         "License :: OSI Approved :: MIT License"
-    ]
+    ],
+    include_package_data=True,
+    zip_safe=False,
 )
