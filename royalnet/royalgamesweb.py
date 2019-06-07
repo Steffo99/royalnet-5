d6ac988a9c1a2d38e4953133392db061845c8c5b
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-06-08 01:33:34 +0200

    tglogin progress

--- royalnet/royalgamesweb.py
+++ royalnet/royalgamesweb.py
@@ -1,13 +1,13 @@
 import os
 from .web import create_app
-from .web.blueprints import home, wikiview
+from .web.blueprints import home, wikiview, tglogin
 
 
 class TestConfig:
     DB_PATH = os.environ["DB_PATH"]
 
 
-app = create_app(TestConfig, [home, wikiview])
+app = create_app(TestConfig, [home, wikiview, tglogin])
 
 
 if __name__ == "__main__":
