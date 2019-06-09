567d0e0979db08fb6378b1935ce1e6ec3b5d5907
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-06-09 18:22:14 +0200

    Get the secret key from envvars

--- royalnet/web/flaskserver.py
+++ royalnet/web/flaskserver.py
@@ -1,5 +1,6 @@
 import typing
 import flask as f
+import os
 from ..database import Alchemy
 from .royalprint import Royalprint
 
@@ -7,6 +8,7 @@ from .royalprint import Royalprint
 def create_app(config_obj: typing.Type, blueprints: typing.List[Royalprint]):
     app = f.Flask(__name__)
     app.config.from_object(config_obj)
+    app.secret_key = os.environ["SECRET_KEY"]
     required_tables = set()
     for blueprint in blueprints:
         required_tables = required_tables.union(blueprint.required_tables)
