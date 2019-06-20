87578155a8e329b1bd67bc1e55a74b6a6155ace8
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-06-20 16:44:05 +0200

    Wiki progress

--- royalnet/web/royalprints/wikiedit/__init__.py
+++ royalnet/web/royalprints/wikiedit/__init__.py
@@ -11,11 +11,16 @@ rp = Royalprint("wikiedit", __name__, url_prefix="/wikiedit", template_folder=tm
                 required_tables={Royal, WikiPage, WikiRevision})
 
 
-@rp.route("/<uuid:page_id>", defaults={"title": ""})
+@rp.route("/<uuid:page_id>", defaults={"title": ""}, methods=["GET", "POST"])
 @rp.route("/<uuid:page_id>/<title>")
 def wikiedit_by_id(page_id: uuid.UUID, title: str):
     alchemy, alchemy_session = f.current_app.config["ALCHEMY"], f.current_app.config["ALCHEMY_SESSION"]
     page = alchemy_session.query(alchemy.WikiPage).filter(alchemy.WikiPage.page_id == page_id).one_or_none()
     if page is None:
         return "No such page", 404
-    return f.render_template("wikiedit_page.html", page=page)
+
+    if f.request.method == "GET":
+        return f.render_template("wikiedit_page.html", page=page)
+
+    elif f.request.method == "POST":
+        return "Haha doesn't work yet"
