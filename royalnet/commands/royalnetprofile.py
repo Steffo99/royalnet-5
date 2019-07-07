0fa2cec5c27326e618390976cd82467343d0402a
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-07-08 02:16:25 +0300

    Fix royalnetprofile command

--- royalnet/commands/royalnetprofile.py
+++ royalnet/commands/royalnetprofile.py
@@ -1,6 +1,6 @@
 import asyncio
 from ..utils import Command, Call
-from ..error import InvalidInputError
+from ..database.tables import Royal, Telegram, Discord
 
 
 class RoyalnetprofileCommand(Command):
@@ -9,9 +9,11 @@ class RoyalnetprofileCommand(Command):
     command_description = "Invia in chat il link al tuo profilo Royalnet!"
     command_syntax = ""
 
+    require_alchemy_tables = {Royal, Telegram, Discord}
+
     @classmethod
     async def common(cls, call: Call):
-        author = await call.get_author()
+        author = await call.get_author(error_if_none=True)
         if author is None:
             await call.reply("⚠️ Devi essere registrato a Royalnet per usare questo comando!")
             return
