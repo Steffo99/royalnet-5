a4cfa42fb0870288765a214bd068ae2add513f46
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-10-19 12:09:10 +0200

    Add VersionCommand

--- royalnet/packs/common/commands/__init__.py
+++ royalnet/packs/common/commands/__init__.py
@@ -1,9 +1,11 @@
 # Imports go here!
 from .ping import PingCommand
+from .version import VersionCommand
 
 # Enter the commands of your Pack here!
 available_commands = [
     PingCommand,
+    VersionCommand
 ]
 
 # Don't change this, it should automatically generate __all__
--- royalnet/version.py
+++ royalnet/version.py
@@ -1,1 +1,1 @@
-semantic = "5.0a66"
+semantic = "5.0a69"
