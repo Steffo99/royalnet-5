4c09e946d15f62eb6dd78cd7fcb13ec9fd2e72b9
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-10-25 15:00:47 +0200

    5.0a73

--- to_pypi.sh
+++ to_pypi.sh
@@ -6,6 +6,7 @@ VERSION=$(python3.7 -m royalnet.version)
 rm -rf dist
 python setup.py sdist bdist_wheel
 twine upload "dist/royalnet-$VERSION"*
-hub commit -m "$VERSION"
-hub push
-hub release create -m "Royalnet $VERSION" "$VERSION"
+git add *
+git commit -m "$VERSION"
+git push
+hub release create --message "Royalnet $VERSION" --prerelease "$VERSION"
