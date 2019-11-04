59191a7ec46f501aaa73cc0b7646c2a241b7680c
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-11-04 12:56:27 +0100

    Fix website!

--- royalnet/packs/common/commands/__init__.py
+++ royalnet/packs/common/commands/__init__.py
@@ -9,4 +9,4 @@ available_commands = [
 ]
 
 # Don't change this, it should automatically generate __all__
-__all__ = [command.__class__.__qualname__ for command in available_commands]
+__all__ = [command.__name__ for command in available_commands]
--- royalnet/packs/royal/tables/__init__.py
+++ royalnet/packs/royal/tables/__init__.py
@@ -32,4 +32,4 @@ available_tables = [
 ]
 
 # Don't change this, it should automatically generate __all__
-__all__ = [table.__class__.__qualname__ for table in available_tables]
+__all__ = [table.__name__ for table in available_tables]
