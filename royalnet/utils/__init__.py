8148e6f44e82ecbf590e705ba709906bfbde77dd
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-11-17 18:58:14 +0100

    Document formatters (and remove splitstring)

--- royalnet/utils/__init__.py
+++ royalnet/utils/__init__.py
@@ -1,24 +1,18 @@
 from .asyncify import asyncify
 from .safeformat import safeformat
 from .sleep_until import sleep_until
-from .formatters import andformat, underscorize, ytdldateformat, numberemojiformat, splitstring, ordinalformat
+from .formatters import andformat, underscorize, ytdldateformat, numberemojiformat, ordinalformat
 from .urluuid import to_urluuid, from_urluuid
 from .multilock import MultiLock
 
 __all__ = [
     "asyncify",
     "safeformat",
-    "cdj",
     "sleep_until",
-    "plusformat",
     "andformat",
-    "plusformat",
     "underscorize",
     "ytdldateformat",
     "numberemojiformat",
-    "telegram_escape",
-    "discord_escape",
-    "splitstring",
     "ordinalformat",
     "to_urluuid",
     "from_urluuid",
