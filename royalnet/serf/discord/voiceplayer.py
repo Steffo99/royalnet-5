e75e1cf5bb1c24ce5eead7e2d74b0bf0819ea4bd
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-02-04 00:24:46 +0100

    Revert voiceplayer change

--- royalnet/serf/discord/voiceplayer.py
+++ royalnet/serf/discord/voiceplayer.py
@@ -87,7 +87,6 @@ class VoicePlayer:
             raise PlayerNotConnectedError()
         if self.voice_client.is_playing():
             raise PlayerAlreadyPlaying()
-        self.playing = None
         log.debug("Getting next AudioSource...")
         next_source: Optional["discord.AudioSource"] = await self.playing.next()
         if next_source is None:
