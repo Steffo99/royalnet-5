7c60046cb7fd5a9a69f87f60c07c8b85896777f0
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-02-11 23:05:45 +0100

    publish: 5.5

--- publish.bat
+++ publish.bat
@@ -1,1 +1,2 @@
-git commit -am "publish: %1" && git push && poetry build && poetry publish && hub release create "%1" -m "Royalnet %1"
+git commit -am "publish: %1"
+git push && poetry build && poetry publish && hub release create "%1" -m "Royalnet %1"
