b11ac76d468de075bf4393785c049c4c471cbfe2
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-05-28 17:05:29 +0200

    Fix publish.bat

--- publish.bat
+++ publish.bat
@@ -1,2 +1,2 @@
 git commit -am "publish: %1"
-git push && poetry build && poetry publish && hub release create "%1" -m "Royalnet %1" && sentry-cli releases set-commits "%1" --auto && sentry-cli releases deploys "%1" new --env production -n "Version %1"
+git push && poetry build && poetry publish && hub release create "%1" -m "Royalnet %1" && sentry-cli releases set-commits "%1" --auto && sentry-cli releases deploys "%1" new --env production -n "Royalnet %1"
