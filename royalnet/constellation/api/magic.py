8f34186b10d727ec1f6ec9da482e28b7bb09ff5f
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-08-18 02:24:43 +0200

    Document magic

--- royalnet/constellation/api/magic.py
+++ royalnet/constellation/api/magic.py
@@ -2,10 +2,7 @@ import functools
 
 
 def magic(func):
-    # i made this at 2:00 am
-    # at 2:15 am i already had no idea on how it worked
-    # at 2:30 i still have no idea but it works appearently
-    # 2:40 TODO: document me
+    """Mark a function as "magic", so its override can be detected externally."""
     func.__magic__ = True
 
     @functools.wraps(func)
